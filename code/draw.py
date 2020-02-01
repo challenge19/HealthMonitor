94ae17bb28c9c970a1c3e97794b0d2d3f14d10e0
Zhesi Shen <challenge19@users.noreply.github.com>
2020-02-01 16:26:50 +0800

    add weight monitor
    add weight monitor

--- code/draw.py
+++ code/draw.py
@@ -12,11 +12,10 @@ import matplotlib.pyplot as plt
 data_file = '../data/health.xlsx'
 figure_dir = '../figure/'
 
-df = pd.read_excel(data_file)
+df = pd.read_excel(data_file,sheetname='temp')
 df['Date'] = pd.to_datetime(df['Date'])
 df.set_index('Date',inplace=True)
 # 绘制体温
-
 fig = plt.figure(figsize=(4,3))
 ax = plt.subplot(111)
 
@@ -30,3 +29,22 @@ ax.set_ylabel('Temp',fontsize=13)
 
 plt.tight_layout()
 plt.savefig(f'{figure_dir}/temp.png',dpi=200)
+
+# 绘制体重
+df = pd.read_excel(data_file,sheetname='weight')
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date',inplace=True)
+
+fig = plt.figure(figsize=(4,3))
+ax = plt.subplot(111)
+
+#ax.plot(df['date'],df['Temp'],'o-',label='Temp')
+df.plot(ls='-',marker='o',ax=ax)
+ax.set_ylim(120,160)
+ax.axhline(y=140,color='k',ls='--')
+
+ax.set_xlabel('Date',fontsize=13)
+ax.set_ylabel('Weight',fontsize=13)
+
+plt.tight_layout()
+plt.savefig(f'{figure_dir}/weight.png',dpi=200)
